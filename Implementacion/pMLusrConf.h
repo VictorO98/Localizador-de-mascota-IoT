6f94a68f32b46068fd317c1e95cad6903725f122
Victor Ospina <ospiman98@gmail.com>
2021-06-13 16:13:53 -0500

    Inicio proyecto implementacion hilos localizador mascota c

---
+++ Implementacion/pMLusrConf.h
@@ -0,0 +1,11 @@
+/*******************************************************************************
+*
+*  pMLusrConf.h -   Manifest Constants and Types for concurrent access to a
+*                   circular buffer modelling a message queue
+*
+*   Notes:          Defined according to the Pet Locator project
+*
+*******************************************************************************/
+
+#define     BUFSIZE     8     /* number of slots in queues */
+#define     NUM_QUEUES  3     /* number of queues */
